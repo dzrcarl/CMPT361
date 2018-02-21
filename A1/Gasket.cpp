d4f4f9901b79dacf27b6835e1f6faa5ab6c1b9d1
dzrcarl <nuclearcarl@hotmail.com>
2018-02-20 22:09:05 -0800

    change lines to white

--- A1/Gasket.cpp
+++ A1/Gasket.cpp
@@ -8,7 +8,7 @@ static vec3  base_colors[] = {
 		vec3( 1.0, 0.5, 0.0 ),
 		vec3( 0.5, 1.0, 0.0 ),
 		vec3( 0.0, 0.0, 1.0 ),
-		vec3( 0.0, 0.0, 0.0 )
+		vec3( 1.0, 1.0, 1.0 )  // white
 };
 
 //three triangles
@@ -58,8 +58,8 @@ init( void )
     points1[0] = vec2( -1.0, 0.0 );
     points1[1] = vec2( 1.0, 0.0 );
 
-    points1[2] = vec2( -1.0, -1.0 );
-    points1[3] = vec2( 1.0, -1.0 );
+    points1[2] = vec2( -1.0, -0.5 );
+    points1[3] = vec2( 1.0, -0.5 );
 
     
     //lines can have color too!
