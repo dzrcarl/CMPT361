571d4aed5ecaa8d96358f057336c6df0666af4d7
dzrcarl <nuclearcarl@hotmail.com>
2018-02-20 20:15:17 -0800

    create game and grid file for tetris and modified gasket for testing

---
+++ A1/data.h
@@ -0,0 +1,15 @@
+#ifndef data
+#define data
+
+#include "angel.h"
+
+// empty block flag
+const int empty = -1;
+
+const int NumOfHB = 20;
+const int NumOfHL = NumOfHB + 1;
+const int NumOfVB = 10;
+const int NumOfVL = NumOfVB + 1;
+
+
+#endif
